52c5685f62dd1643330bbab50038a0cac81f9510
Ashley Clarke <crashley92@gmail.com>
2020-02-27 21:33:38 -0800

    adjusting config for heroku deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors')
 const path = require('path');
 const proxy = require('http-proxy-middleware')
+const fs = require('fs')
+
 
 
 const app = express();
