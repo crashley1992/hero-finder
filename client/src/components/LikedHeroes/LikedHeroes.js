899a643832a599216599093410921115d3b4c382
Ashley Clarke <crashley92@gmail.com>
2020-03-24 21:19:47 -0700

    working on adding the comic search button/functionality

--- client/src/components/LikedHeroes/LikedHeroes.js
+++ client/src/components/LikedHeroes/LikedHeroes.js
@@ -7,9 +7,12 @@ const LikedCharacters = (props) => {
         <Card.Img variant="top" src={props.link} />
         <Card.Body>
             <Card.Title><strong>{props.name}</strong></Card.Title>
+            {/* DELETE BUTTON */}
             <button className="btn btn-danger" id={props.id} onClick={(event) => {
                 props.handleDelete(event.target.id)
             }}>Remove</button>
+            {/* FIND COMIC BUTTON */}
+            <button className='btn btn-success'>Find Comics</button>
         </Card.Body>
 </Card>
     )
